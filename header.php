986e706d06c3c4da69bbc887eeee7a3c18d9fbcb
Mukeshkumar <mukeshkumar87086@gmail.com>
2025-08-12 18:23:27 +0530

    selva

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ if (isset($page)) {
         <!--------------Top bar ends here------------------>
 
         <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container my-3"> <a class="navbar-brand logo" href="index.php"><img src="">BeafBai</a>
+            <div class="container my-3"> <a class="navbar-brand logo" href="index.php"><img src=""></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
